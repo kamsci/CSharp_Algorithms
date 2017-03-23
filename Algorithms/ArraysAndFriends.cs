c72ce7c2cb126faca3512362c895f207d01469ff
Krista Calderon <kamsci@gmail.com>
2017-03-22 20:03:33 -0700

    Added Array class and one method

---
+++ Algorithms/ArraysAndFriends.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class ArraysAndFriends
+    {
+        /* Given an int array, create a new array of the product of all numbers not in the current index.
+         * ie. source: [2, 4, 3, 6] -> [4*3*6, 2*3*6, 2*4*6, 2*4*3] -> [72, 36, 48, 24]
+         */
+         public static int[] ArrayMultiply(int[] intArr)
+        {
+            List<int> result = new List<int>();
+            int product = 1;
+
+            // go through array the first time multiplying by everything to the right
+            for(int i = 0; i < intArr.Length; i++)
+            {
+                result.Add(product);
+                product = product * intArr[i];
+            }
+            product = 1;
+            // go through array a second time backwards multiplying by everything to the left
+            for(int k = intArr.Length - 1; k >= 0; k--)
+            {
+                result[k] = result[k] * product;
+                product = product * intArr[k];
+            }
+            return result.ToArray();
+        }
+    }
+    // 1 array to manage 3 stacks
+}
---
+++ UnitTestAlgorithms/ArraysAndFriends_UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+
+namespace UnitTestAlgorithms
+{
+    [TestClass]
+    public class ArraysAndFriends_UnitTests
+    {
+        [TestMethod]
+        public void ArrayMultiply_Test()
+        {
+            // Arrange
+            int[] arr = new int[] { 2, 4, 3, 6 };
+
+            // Act
+            int[] result = ArraysAndFriends.ArrayMultiply(arr);
+            int[] expected = new int[] { 72, 36, 48, 24 };
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
